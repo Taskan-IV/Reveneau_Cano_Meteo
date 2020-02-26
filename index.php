8610d4c06a7faf4b8343b8c97d90d7d2f842ab19
Jarod <jarod.reveneau@gmail.com>
2020-02-26 11:09:47 +0100

    Modif amCharts

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="fr">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <title>Test amCharts</title>
+  </head>
+  <body>
+    <div id="chartdiv" style="width: 900px; height: 800px;"></div>
+  </body>
+  <footer>
+    <script src="./lib/amcharts4/core.js"></script>
+    <script src="./lib/amcharts4/charts.js"></script>
+    <script src="./lib/amcharts4/maps.js"></script>
+    <script src="./lib/amcharts4/geodata/franceHigh.js"></script>
+    <script src="./js.js"></script>
+  </footer>
 </html>
