2cd07220dfd24cc87d50b2d4ddff08f58a859f46
Jarod <jarod.reveneau@gmail.com>
2020-02-26 15:50:40 +0100

    ajout liste

--- index.php
+++ index.php
@@ -3,18 +3,23 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
     <title>Test amCharts</title>
   </head>
   <body>
+    <select name="jour" id="jour" onchange="updateMap();">
+    	<option value="1" selected="yes" >1</option>
+      <option value="2">2</option>
+    </select>
     <div id="chartdiv" style="width: 900px; height: 800px;"></div>
-    <div id="1"></div>
-    <div id="2"></div>
   </body>
   <footer>
     <script src="./lib/amcharts4/core.js"></script>
     <script src="./lib/amcharts4/charts.js"></script>
     <script src="./lib/amcharts4/maps.js"></script>
     <script src="./lib/amcharts4/geodata/franceHigh.js"></script>
+    <script src="./lib/jquery.js"></script>
     <script src="./js.js"></script>
+    <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
   </footer>
 </html>
--- js.js
+++ js.js
@@ -1,47 +1,60 @@
-getJSONMeteo(2);
+parseJSONMeteo(1);
+
 function createMap(tabMeteo) {
   // Création de l'instance d'une carte
   var chart = am4core.create("chartdiv", am4maps.MapChart);
-  // On set le fait que c'est une map de france
+
+  // On set le fait que c'est une carte de france
   chart.geodata = am4geodata_franceHigh;
-  // Set projection
   chart.projection = new am4maps.projections.Miller();
 
   // Create map polygon series
   var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-  // Make map load polygon (like country names) data from GeoJSON
   polygonSeries.useGeodata = true;
 
   // Configure series
   var polygonTemplate = polygonSeries.mapPolygons.template;
   polygonTemplate.tooltipText = "{name}";
   polygonTemplate.fill = am4core.color("#74B266");
+  polygonTemplate.propertyFields.fill = "fill";
 
   // Create hover state and set alternative fill color
   var hs = polygonTemplate.states.create("hover");
   hs.properties.fill = am4core.color("#367B25");
 
+  // Ici on créé les labels de température qui s'affiche sur la carte
   var imageSeries = chart.series.push(new am4maps.MapImageSeries());
   var imageSeriesTemplate = imageSeries.mapImages.template;
-  var label = imageSeriesTemplate.createChild(am4core.Label);
-  label.text = "{t}";
-
   imageSeriesTemplate.propertyFields.latitude = "lat";
   imageSeriesTemplate.propertyFields.longitude = "lng";
-  // Add data
+
+  var label = imageSeriesTemplate.createChild(am4core.Label);
+  label.text = "{temp}";
   imageSeries.data = tabMeteo;
-  polygonTemplate.propertyFields.fill = "fill";
 }
 
-
-function getJSONMeteo(jour){
+function parseJSONMeteo(jour) {
   var requestURL = 'http://localhost/Reveneau_Cano_Meteo/Data/meteo.json';
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
   request.send();
   request.onload = function() {
-    createMap(request.response[jour - 1].station);
+    var station = request.response[jour - 1].station;
+    var result = station.map(function(ligne) {
+      return {
+        lat : ligne.lat,
+        lng : ligne.lng,
+        ville : ligne.n,
+        temp : (ligne.t / 100).toFixed(2)
+      };
+    });
+    createMap(result);
   }
+}
+
+function updateMap(){
+  var jour = document.getElementById("jour");
+  var val = jour.options[jour.selectedIndex].value;
+  parseJSONMeteo(val);
 }
