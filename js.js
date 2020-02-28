f997470462487c798106cab5d12b4ef3f4b2cc64
Jarod <jarod.reveneau@gmail.com>
2020-02-28 09:26:23 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,39 +1,54 @@
-parseJSONMeteo(1);
+// Au départ on créé la carte de la france sans les données //
+// Création de l'instance d'une carte
+var chart = am4core.create("chartdiv", am4maps.MapChart);
 
-function createMap(tabMeteo) {
-  // Création de l'instance d'une carte
-  var chart = am4core.create("chartdiv", am4maps.MapChart);
+// On set le fait que c'est une carte de france
+chart.geodata = am4geodata_franceHigh;
+chart.projection = new am4maps.projections.Miller();
 
-  // On set le fait que c'est une carte de france
-  chart.geodata = am4geodata_franceHigh;
-  chart.projection = new am4maps.projections.Miller();
+// Create map polygon series
+var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+polygonSeries.useGeodata = true;
 
-  // Create map polygon series
-  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-  polygonSeries.useGeodata = true;
+// Configure series
+var polygonTemplate = polygonSeries.mapPolygons.template;
+polygonTemplate.tooltipText = "{name}";
+polygonTemplate.fill = am4core.color("#74B266");
+polygonTemplate.propertyFields.fill = "fill";
 
-  // Configure series
-  var polygonTemplate = polygonSeries.mapPolygons.template;
-  polygonTemplate.tooltipText = "{name}";
-  polygonTemplate.fill = am4core.color("#74B266");
-  polygonTemplate.propertyFields.fill = "fill";
+// Create hover state and set alternative fill color
+var hs = polygonTemplate.states.create("hover");
+hs.properties.fill = am4core.color("#367B25");
 
-  // Create hover state and set alternative fill color
-  var hs = polygonTemplate.states.create("hover");
-  hs.properties.fill = am4core.color("#367B25");
+// Ici on créé les labels de température qui s'affiche sur la carte
+var imageSeries = chart.series.push(new am4maps.MapImageSeries());
+var imageSeriesTemplate = imageSeries.mapImages.template;
+imageSeriesTemplate.propertyFields.latitude = "lat";
+imageSeriesTemplate.propertyFields.longitude = "lng";
 
-  // Ici on créé les labels de température qui s'affiche sur la carte
-  var imageSeries = chart.series.push(new am4maps.MapImageSeries());
-  var imageSeriesTemplate = imageSeries.mapImages.template;
-  imageSeriesTemplate.propertyFields.latitude = "lat";
-  imageSeriesTemplate.propertyFields.longitude = "lng";
+// Ici on affiche les labels de température
+var label = imageSeriesTemplate.createChild(am4core.Label);
+label.text = "{temp}";
+label.fontSize = 20;
 
-  var label = imageSeriesTemplate.createChild(am4core.Label);
-  label.text = "{temp}";
-  imageSeries.data = tabMeteo;
-  label.fontSize = 20;
-}
+// Ici on affiche les station et leur nom
+var circle = imageSeriesTemplate.createChild(am4core.Circle);
+circle.radius = 4;
+circle.fill = am4core.color("#B27799");
+circle.stroke = am4core.color("#FFFFFF");
+circle.strokeWidth = 2;
+circle.nonScaling = true;
+circle.tooltipText = "{ville}";
+circle.events.on("hit", afficherStation({heure}));
 
+ // La on affiche les données du jour 0
+updateMap();
+
+/* -----------------------------------------------------------------------*
+ * ----------- Les méthodes utiles pour l'affichage des données ----------*
+ * -----------------------------------------------------------------------*/
+
+ // Cette fonction vas chercher toute les stations pour le jour "jour"
 function parseJSONMeteo(jour) {
   var requestURL = 'http://localhost/Reveneau_Cano_Meteo/Data/meteo.json';
   var request = new XMLHttpRequest();
@@ -47,15 +62,21 @@ function parseJSONMeteo(jour) {
         lat : ligne.lat,
         lng : ligne.lng,
         ville : ligne.n,
-        temp : (ligne.t / 100).toFixed(2)
+        temp : (ligne.t / 100).toFixed(2),
+        heure : ligne.hours
       };
     });
-    createMap(result);
+    imageSeries.data = result;
   }
 }
 
+// La on met-à-jour la carte
 function updateMap(){
   var jour = document.getElementById("formControlRange");
   var val = jour.value;
   parseJSONMeteo(val);
+}
+
+function afficherStation() {
+
 }
